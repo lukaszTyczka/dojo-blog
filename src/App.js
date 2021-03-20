19514de62cc279b4f734fc1229a76472b5af3339
Łukasz Tyczka <lukasz@tyczka.net>
2021-03-20 17:42:59 +0100

    Add Bloglist and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const title = 'Welcome to the new blog';
+    // const likes = 50;
+    // const person = { name: 'Luke', age: 30 };
+    // const link = 'http://www.google.com';
+    return (
+        <div className="App">
+            <Navbar />
+            <div className="content">
+                <Home />
+            </div>
+        </div>
+    );
 }
 
 export default App;
