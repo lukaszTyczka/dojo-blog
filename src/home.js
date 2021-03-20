150406ba1b177aef10c7102ea5f57fdc78f118c4
Łukasz Tyczka <lukasz@tyczka.net>
2021-03-20 18:49:17 +0100

    fetching data and json-server

--- src/home.js
+++ src/home.js
@@ -1,30 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './blogList';
-
+// npx json-server --watch data/db.json --port 8000   -> set json-server
 const Home = () => {
-    const [blogs] = useState([
-        {
-            title: 'My new website',
-            body: 'lorem ipsum...',
-            author: 'mario',
-            id: 1,
-        },
-        {
-            title: 'Welcome party!',
-            body: 'lorem ipsum...',
-            author: 'yoshi',
-            id: 2,
-        },
-        {
-            title: 'Web dev top tips',
-            body: 'lorem ipsum...',
-            author: 'mario',
-            id: 3,
-        },
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                });
+        }, 1000);
+    }, []);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs" />
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All blogs" />}
         </div>
     );
 };
