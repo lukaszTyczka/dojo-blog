7de9db67ddb8a884fe6c6fe1ef66d1e30f644e12
Łukasz Tyczka <lukasz@tyczka.net>
2021-03-21 20:31:10 +0100

    add error service in home

--- src/home.js
+++ src/home.js
@@ -4,22 +4,35 @@ import BlogList from './blogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then((res) => {
+                    if (!res.ok) {
+                        throw Error(
+                            'Could not fetch the data for that resource'
+                        );
+                    }
                     return res.json();
                 })
                 .then((data) => {
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setError(err.message);
+                    setIsPending(false);
+                    setBlogs(null);
                 });
         }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs" />}
         </div>
